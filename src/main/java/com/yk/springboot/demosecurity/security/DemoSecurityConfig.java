b08262182672602f0a2c77b32818a9707a36d3ae
Yogesh <yogeshkhot2007@gmail.com>
2023-06-03 19:55:31 +0530

    first commit springboot mvc security

---
+++ src/main/java/com/yk/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.yk.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder().username("john")
+                .roles("EMPLOYE")
+                .password("{noop}test1234")
+                .build();
+        UserDetails mary = User.builder().username("mary")
+                .roles("EMPLOYE", "MANAGER")
+                .password("{noop}test1234")
+                .build();
+        UserDetails susan = User.builder().username("susan")
+                .roles("EMPLOYE", "MANAGER", "ADMIN")
+                .password("{noop}test1234")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll());
+        return http.build();
+    }
+
+}
